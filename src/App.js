4b735beaa7fc481e854ad4292737840a30fba336
gero_moroni <geromoroniii@hotmail.com>
2021-09-15 10:34:00 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Nav from "./NavBar"
+
+
+const App = () => {
+return(
+     <header className="test">
+          <h1>Tienda de ropa</h1>
+             <Nav />
+     </header>
+)
+}
+export default App
---
+++ src/NavBar.js
@@ -0,0 +1,14 @@
+import "./style.css"
+const Nav = () => {
+    return (
+        <nav>
+            <h1>Mi tienda</h1>
+            <a href="./NavBar.js">Accesorios</a>
+            <a href="./NavBar.js">Remeras</a>
+            <a href="./NavBar.js">Zapatillas</a>
+            <a href="./NavBar.js">Buzos</a>
+        </nav>
+    )
+}
+
+export default Nav
